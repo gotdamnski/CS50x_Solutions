0037dab424c3d6704b7fe42eac9d3d291985bf51
gotdamnski <137406501+gotdamnski@users.noreply.github.com>
2025-09-29 16:51:55 +0200

    add initial solution for mario-less problem

---
+++ Week1/mario/mario-less.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <cs50.h>
+
+void print_space(int n);
+void print_hash(int n);
+
+int main(void)
+{
+    int pyramid_height;
+    do
+    {
+        pyramid_height = get_int("Hight: ");
+    }
+    while (pyramid_height < 1 || pyramid_height > 8);
+
+    for (int row = 1; row <= pyramid_height; row++)
+    {
+        print_space(pyramid_height - row);
+        print_hash(row);
+        printf("\n");
+    }
+}
+
+void print_space(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf(" ");
+    }
+}
+
+void print_hash(int n)
+{
+    for (int k = 0; k < n; k++)
+    {
+        printf("#");
+    }
+}
+
