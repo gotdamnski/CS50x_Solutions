9d7d43b4140b2d030889454791c7339082f332bb
gotdamnski <137406501+gotdamnski@users.noreply.github.com>
2025-10-05 13:29:35 -0300

    get rid of magic numbers

--- Week1/cash-n-credit/cash.c
+++ Week1/cash-n-credit/cash.c
@@ -1,13 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
+#define QUARTER 25
+#define DIME 10
+#define NICKEL 5
+#define PENNY 1
+
 
 int main(void)
 {
     int change_owed;
-    int quarter = 25;
-    int dime = 10;
-    int nickel = 5;
-    int penny = 1;
     int counter = 0;
     do
     {
@@ -17,24 +18,24 @@ int main(void)
 
     while (change_owed > 0)
     {
-        if (change_owed - quarter >= 0)
+        if (change_owed - QUARTER >= 0)
         {
-            change_owed = change_owed - quarter;
+            change_owed = change_owed - QUARTER;
             counter++;
         }
-        else if (change_owed - dime >= 0)
+        else if (change_owed - DIME >= 0)
         {
-            change_owed = change_owed - dime;
+            change_owed = change_owed - DIME;
             counter++;
         }
-        else if (change_owed - nickel >= 0)
+        else if (change_owed - NICKEL >= 0)
         {
-            change_owed = change_owed - nickel;
+            change_owed = change_owed - NICKEL;
             counter++;
         }
-        else if (change_owed - penny >= 0)
+        else if (change_owed - PENNY >= 0)
         {
-            change_owed = change_owed - penny;
+            change_owed = change_owed - PENNY;
             counter++;
         }
     }
